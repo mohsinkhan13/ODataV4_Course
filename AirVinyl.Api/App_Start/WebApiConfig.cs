7707a6aa30d59f5a5a1a9264dd594b1336818716
mohsink13 <mohsink13@gmail.com>
2017-04-25 15:47:44 +0100

    ODataControllers and Helpers to get PropertyValues

---
+++ AirVinyl.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Http;
+using System.Web.OData.Extensions;
+using Microsoft.OData.Edm;
+using System.Web.OData.Builder;
+using AirVinyl.Model;
+
+namespace AirVinyl.Api
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapODataServiceRoute("ODataRoute", "odata", GetEdmModel());
+            config.EnsureInitialized();
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.Namespace = "AirVinyl";
+            builder.ContainerName = "AirVinylContainer";
+
+            builder.EntitySet<Person>("People");
+            builder.EntitySet<VinylRecord>("VinylRecords");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
